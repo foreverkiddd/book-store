90c12cd6181233b980d839d472622fdf34c10ca8
JungEun <foreverkiddd@gmail.com>
2025-04-29 18:01:23 +0900

    250429 메인 화면 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,41 @@
-import Button from "../components/common/Button";
-import InputText from "../components/common/InputText";
-import Title from "../components/common/Title";
+import styled from "styled-components";
+import MainReview from "@/components/main/MainReview";
+import { useMain } from "@/hooks/useMain";
+import Title from "@/components/common/Title";
+import MainNewBooks from "@/components/main/MainNewBooks";
+import MainBest from "@/components/main/MainBest";
+import Banner from "@/components/common/banner/Banner";
 
 function Home() {
+  const { reviews, newBooks, bestBooks, banners } = useMain();
+
   return (
-    <>
-      <div>home body</div>
-    </>
+    <HomeStyle>
+      {/* 배너 */}
+      <Banner banners={banners} />
+      {/* 베스트셀러 */}
+      <section className="section">
+        <Title size="large">베스트셀러</Title>
+        <MainBest books={bestBooks} />
+      </section>
+      {/* 신간 */}
+      <section className="section">
+        <Title size="large">신간</Title>
+        <MainNewBooks books={newBooks} />
+      </section>
+      {/* 리뷰 */}
+      <section className="section">
+        <Title size="large">리뷰</Title>
+        <MainReview reviews={reviews} />
+      </section>
+    </HomeStyle>
   );
 }
+
+const HomeStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
 
 export default Home;
